39a456dc6f227d28f3d9a11e90bd4ed83633c0fe
enesarihan <enessarihan35@icloud.com>
2025-04-19 01:41:14 +0300

    Footer has been updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,31 @@
+import Link from "next/link";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <footer className="w-full py-4 border-t border-gray-200 text-center absolute bottom-0">
-      <p className="text-sm text-gray-500">
-        All rights reserved © {new Date().getFullYear()} Enes SARIHAN
-      </p>
+    <footer className="absolute w-full py-4 border-t border-gray-200 items-center bottom-0">
+      <div className="container mx-auto flex items-center">
+        <p className="text-sm text-gray-500 text-center flex-grow">
+          All rights reserved © {new Date().getFullYear()} Enes SARIHAN
+        </p>
+        <div className="flex justify-end items-center">
+          <Link href={"https://www.instagram.com/enesarihan/"} className="ml-4">
+            {" "}
+            <FaInstagram size={20} />
+          </Link>
+          <Link href={"https://github.com/enesarihan"} className="ml-4">
+            {" "}
+            <FaGithub size={20} />
+          </Link>
+          <Link
+            href={"https://www.linkedin.com/in/enesarihan/"}
+            className="ml-4"
+          >
+            {" "}
+            <FaLinkedin size={20} />
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 };
