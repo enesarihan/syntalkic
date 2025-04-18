a1cf501f62ddec23f234c82e74e9e7cf6a01c487
enesarihan <enessarihan35@icloud.com>
2025-04-19 01:09:49 +0300

    Implement GeneratedSyntalkic and NotFound pages; refactor Button component and utility functions for improved functionality and styling

---
+++ app/(root)/syntalkic/[id]/page.tsx
@@ -0,0 +1,38 @@
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.actions";
+import { getSyntalkicById } from "@/lib/actions/general.action";
+import { redirect } from "next/navigation";
+
+const GeneratedSyntalkic = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  const user = await getCurrentUser();
+  const syntalkic = await getSyntalkicById(id);
+
+  if (!syntalkic) redirect("/");
+
+  return (
+    <>
+      <div className="flex flex-row gap-4 justify-between">
+        <div className="flex flex-col gap-4 text-center">
+          <div className="flex flex-row gap-4 items-center">
+            <h3 className="capitalize">Syntalkic as {syntalkic.role}</h3>
+          </div>
+          <p className="bg-gray-400 dark:bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize">
+            {syntalkic.topic}
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <Agent
+          userName={user?.name || ""}
+          type="syntalkic"
+          syntalkicId={id}
+          userId={user?.id}
+          questions={syntalkic.questions}
+        />
+      </div>
+    </>
+  );
+};
+
+export default GeneratedSyntalkic;
---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import { NotFoundPage } from "@/components/ui/404-page-not-found";
+
+const NotFound = () => {
+  return (
+    <>
+      <NotFoundPage />
+    </>
+  );
+};
+
+export default NotFound;
